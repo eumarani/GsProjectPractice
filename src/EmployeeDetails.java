89005d2e4b107483bf1bcd79ebf3e93188e46f57
Umarani <eumarani456@gmail.com>
2025-01-12 20:33:30 +0530

    algo

--- src/EmployeeDetails.java
+++ src/EmployeeDetails.java
@@ -49,7 +49,8 @@ public class EmployeeDetails {
                 new Employee(50000,"Jake", 40),
                 new Employee(60000,"Sarah", 30),
                 new Employee(90000,"Tom", 39),
-                new Employee(48000,"Lucy", 48)
+                new Employee(48000,"Lucy", 48),
+                new Employee(28000,"Lucy", 18)
         );
 
         System.out.println(employees);
@@ -99,5 +100,53 @@ public class EmployeeDetails {
                 .filter(n->n.getAge()>40)
                 .collect(Collectors.averagingInt(Employee::getAge));
         System.out.println("age is more than 40 avg:" +average);
+        //Get all employees who are older than 25 and earn more than 50,000.
+       List<Employee> Ageg25Sm5k= employees.stream()
+                .filter(e->e.getAge()>25 && e.getSalary()>50000)
+                .collect(Collectors.toList());
+       System.out.println("Age is older than 25 and earn more than 50,000:" +Ageg25Sm5k);
+
+        //Create a new list containing the names of the filtered employees, converted to uppercase
+           List<String> nameToUpper= filteredresult.stream()
+                    .map(e->e.getName().toUpperCase())
+                    .collect(Collectors.toList());
+
+           System.out.println("names in uppercase:");
+           nameToUpper.forEach(System.out::println);
+
+        //Sort the names of the employees in reverse alphabetical order.
+        List<String> sortedNames=employees.stream()
+                .map(n->n.getName().toUpperCase())
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        System.out.println("Reverse order sorted:");
+        sortedNames.forEach(System.out::println);
+        //Ensure there are no duplicate names in the list.
+        List<String> list1=employees.stream()
+                .map(n->n.getName())
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println("Distinct names:");
+        list1.forEach(n->System.out.print(n+" "));
+   System.out.println("-----------------------");
+        //Limit the list to the first 5 employees after sorting
+        List<Employee> limit5=employees.stream()
+                .sorted(Comparator.comparing(Employee::getName))
+                .limit(5)
+                .collect(Collectors.toList());
+        System.out.println("After asec limit 5:");
+        System.out.println(limit5);
+       // Calculate the total salary of employees whose age is greater than 30.
+  double sum= employees.stream()
+                .filter(n->n.getAge()>30)
+                .mapToDouble(Employee::getSalary)
+                .reduce(0,(a,b)->(a+b));
+    System.out.println("Sum of the salaries who's age is more than 30:"+sum);
+        //Collect the names of employees into a single List<String> and print the list.
+             List<String> nameString=employees.stream()
+                     .map(Employee::getName)
+                     .collect(Collectors.toList());
+             System.out.println(nameString);
+
     }
 }
